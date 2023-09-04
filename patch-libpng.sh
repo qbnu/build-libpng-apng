5ee751c7195e061d3ad82e43e6dcecca10f3afd4
qbnu <93988953+qbnu@users.noreply.github.com>
2023-09-03 22:37:12 -0400

    update libpng

--- patch-libpng.sh
+++ patch-libpng.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # apng support
-patch -d libpng -p1 < libpng-apng/libpng-1.6.39-apng.patch
+patch -d libpng -p1 < libpng-apng/libpng-1.6.40-apng.patch
 
 # 64-bit support
 patch -d libpng -p1 < libpng-x64.patch
